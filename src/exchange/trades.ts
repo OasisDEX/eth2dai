80ad01ccaeaa8f057177f73f36b5b9c81dd5e65e
Maciek Kamiński <aundumla@gmail.com>
2019-01-17 16:42:44 +0100

    Trim down vulcan0x request

--- src/exchange/trades.ts
+++ src/exchange/trades.ts
@@ -1,10 +1,8 @@
 import { BigNumber } from 'bignumber.js';
-import { bindNodeCallback, merge, Observable } from 'rxjs';
-import { map, reduce, switchMap, take } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { NetworkConfig } from '../blockchain/config';
-import { onEveryBlock$ } from '../blockchain/network';
-import { amountFromWei } from '../blockchain/utils';
 import { vulcan0x } from '../blockchain/vulcan0x';
 import { web3 } from '../blockchain/web3';
 
@@ -30,7 +28,7 @@ export interface Trade {
 // TODO: move to all trades?
 const parseTrade = (
   account: string,
-  buyTokenGem: string,
+  buyToken: string,
   baseToken: string,
   quoteToken: string,
 ) => {
@@ -39,14 +37,14 @@ const parseTrade = (
     maker,
     lotAmt,
     bidAmt,
-    bidGem,
+    bidTkn,
     block,
     time,
     tx,
   }: any): Trade => {
     const sellAmount = new BigNumber(lotAmt);
     const buyAmount = new BigNumber(bidAmt);
-    return {...buyTokenGem === bidGem ?
+    return {...buyToken === bidTkn ?
     {
       price: buyAmount.div(sellAmount),
       baseAmount: sellAmount,
@@ -69,135 +67,28 @@ const parseTrade = (
   };
 };
 
-export const getTradesFallback = (
-  context: NetworkConfig,
-  baseToken: string,
-  quoteToken: string,
-  filters: {account?: string, from?: Date, to?: Date},
-): Observable<Trade[]> => {
-  return (filters.account ?
-    merge(
-      getTradesFromBlockchain(
-        context, [baseToken, quoteToken], baseToken, quoteToken, filters.from, filters.to,
-        { maker: filters.account }
-      ),
-      getTradesFromBlockchain(
-        context, [quoteToken, baseToken], baseToken, quoteToken, filters.from, filters.to,
-        { maker: filters.account }
-      ),
-      getTradesFromBlockchain(
-        context, [baseToken, quoteToken], baseToken, quoteToken, filters.from, filters.to,
-        { taker: filters.account }
-      ),
-      getTradesFromBlockchain(
-        context, [quoteToken, baseToken], baseToken, quoteToken, filters.from, filters.to,
-        { taker: filters.account }
-      ),
-    ) :
-    merge(
-      getTradesFromBlockchain(
-        context, [baseToken, quoteToken], baseToken, quoteToken, filters.from, filters.to
-      ),
-      getTradesFromBlockchain(
-        context, [quoteToken, baseToken], baseToken, quoteToken, filters.from, filters.to
-      ),
-    )
-  ).pipe(
-    reduce((a: Trade[], e: Trade[]) => a.concat(e), []),
-    map(trades => trades.filter(t => t.baseAmount.gt(1e-9) && t.quoteAmount.gt(1e-9))),
-    map(trades => trades.sort(compareTrades)),
-  );
-};
-
-const getTradesFromBlockchain = (
-  context: NetworkConfig,
-  tokenPair: string[],
-  baseToken: string,
-  quoteToken: string,
-  from: Date | undefined,
-  to: Date | undefined,
-  filters: { [key: string]: string } = {},
-): Observable<Trade[]> => {
-  const pair = web3.sha3(
-    tokenPair.map(token => context.tokens[token].address.replace(/^0x/, '')).join(''),
-    { encoding: 'hex' }
-  );
-  return onEveryBlock$.pipe(
-    take(1),
-    switchMap(block => {
-      const fromBlock = Math.max(
-        block - (
-          from ?
-          Math.floor(
-            (new Date().getTime() - from.getTime()) / 1000 / 3600 / 24 * context.avgBlocksPerDay
-          ) :
-          30 * context.avgBlocksPerDay
-        ),
-        context.startingBlock,
-      );
-      const toBlock = to ? block - Math.floor(
-        (new Date().getTime() - to.getTime()) / 1000 / 3600 / 24 * context.avgBlocksPerDay
-      ) : 'latest';
-      const filter = context.otc.contract.LogTake(
-        {
-          pair,
-          ...filters,
-        },
-        {
-          fromBlock,
-          toBlock,
-        },
-      );
-      return bindNodeCallback(filter.get.bind(filter))().pipe(
-        map(events => events.map(
-          ({
-            args: { id, maker, give_amt, take_amt, pay_gem, timestamp },
-            blockNumber, transactionHash,
-          }) => {
-            return parseTrade(
-              filters.maker || filters.taker,
-              context.tokens[baseToken].address, baseToken, quoteToken
-            )({
-              maker,
-              offerId: id.toString(),
-              bidAmt: amountFromWei(give_amt, tokenPair[1]).toString(),
-              lotAmt: amountFromWei(take_amt, tokenPair[0]).toString(),
-              bidGem: pay_gem,
-              block: blockNumber, time: timestamp.toNumber() * 1000, tx: transactionHash,
-            });
-          }
-        )),
-      );
-    }),
-  );
-};
-
 export const getTrades = (
   context: NetworkConfig,
   baseToken: string,
   quoteToken: string,
   filters: {account?: string, from?: Date, to?: Date},
 ): Observable<Trade[]> => {
-  const baseTokenAddress = context.tokens[baseToken].address;
-  const quoteTokenAddress = context.tokens[quoteToken].address;
-  const baseTokenGem = (web3 as any).toChecksumAddress(baseTokenAddress);
-  const quoteTokenGem = (web3 as any).toChecksumAddress(quoteTokenAddress);
   const accountVal = filters.account ? (web3 as any).toChecksumAddress(filters.account) : null;
 
-  const fields = ['offerId', 'maker', 'bidAmt', 'bidGem', 'lotAmt', 'block', 'time', 'tx'];
+  const fields = ['offerId', 'maker', 'bidAmt', 'bidTkn', 'lotAmt', 'block', 'time', 'tx'];
   const order = 'TIME_DESC';
   const filter = {
     or: [
       {
         and: [
-          { lotGem: { equalTo: baseTokenGem } },
-          { bidGem: { equalTo: quoteTokenGem } },
+          { lotTkn: { equalTo: baseToken } },
+          { bidTkn: { equalTo: quoteToken } },
         ],
       },
       {
         and: [
-          { lotGem: { equalTo: quoteTokenGem } },
-          { bidGem: { equalTo: baseTokenGem } },
+          { lotTkn: { equalTo: quoteToken } },
+          { bidTkn: { equalTo: baseToken } },
         ],
       },
     ],
@@ -218,7 +109,7 @@ export const getTrades = (
   };
 
   return vulcan0x(context.oasisDataService.url, 'allOasisTrades', filter, fields, order).pipe(
-    map(trades => trades.map(parseTrade(accountVal, quoteTokenGem, baseToken, quoteToken)))
+    map(trades => trades.map(parseTrade(accountVal, quoteToken, baseToken, quoteToken)))
   );
 };
 
