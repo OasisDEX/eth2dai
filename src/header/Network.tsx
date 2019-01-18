8f7b1c09ad32def49aed633ad928cd64229d07e6
Pex <georgi.h.georgieff@gmail.com>
2019-01-18 15:17:43 +0100

    Improve styling

--- src/header/Network.tsx
+++ src/header/Network.tsx
@@ -3,9 +3,11 @@ import { NetworkConfig } from '../blockchain/config';
 import * as styles from './Header.scss';
 
 export class Network extends React.Component<NetworkConfig, any> {
+
   public render() {
+    const network = this.props.name as 'kovan' || 'main';
     return (
-      <div className={`${styles.networkIndicator} Header_${this.props.name}`}>
+      <div className={`${styles.networkIndicator} ${styles[network]}`}>
         {this.props.name}
       </div>
     );
