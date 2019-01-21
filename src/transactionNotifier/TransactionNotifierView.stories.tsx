c48cd926169c3bcd458b901272b02211e90a9477
Krzysztof Kaczor <krzkaczor@gmail.com>
2019-01-21 15:45:15 +0100

    more stories

--- src/transactionNotifier/TransactionNotifierView.stories.tsx
+++ src/transactionNotifier/TransactionNotifierView.stories.tsx
@@ -1,7 +1,20 @@
 import { storiesOf } from '@storybook/react';
 import * as React from 'react';
-import { approveWallet } from '../blockchain/calls/approveCalls';
+
+import { approveWallet, disapproveWallet } from '../blockchain/calls/approveCalls';
+import {
+  CancelData,
+  cancelOffer,
+  offerMake,
+  OfferMakeData,
+  offerMakeDirect,
+  OfferMakeDirectData,
+} from '../blockchain/calls/offerMake';
+import { unwrap, wrap } from '../blockchain/calls/wrapUnwrapCalls';
 import { TxState, TxStatus } from '../blockchain/transactions';
+import { OfferType } from '../exchange/orderbook/orderbook';
+import { OfferMatchType } from '../utils/form';
+import { zero } from '../utils/zero';
 import { Notification } from './TransactionNotifierView';
 
 const tx: TxState = {
@@ -17,4 +30,55 @@ const tx: TxState = {
 
 const stories = storiesOf('TransactionNotifierView', module);
 
-stories.add('Notification', () => <Notification {...tx} />);
+stories.add('Notification - approve transfer', () => <Notification {...tx} />);
+
+stories.add('Notification - disapprove transfer', () => (
+  <Notification {...{ ...tx, meta: { ...disapproveWallet, args: { token: 'WETH' } } }} />
+));
+
+stories.add('Notification - cancel offer', () => (
+  <Notification {...{ ...tx, meta: { ...cancelOffer, args: { offerId: zero } as CancelData } }} />
+));
+
+const offerMakeData: OfferMakeData = {
+  buyAmount: zero,
+  buyToken: 'WETH',
+  sellAmount: zero,
+  sellToken: 'WETH',
+  matchType: OfferMatchType.limitOrder,
+  position: zero,
+  kind: OfferType.buy,
+  gasPrice: zero,
+};
+
+const offerMakeDirectData: OfferMakeDirectData = {
+  baseAmount: zero,
+  baseToken: 'WETH',
+  quoteAmount: zero,
+  quoteToken: 'WETH',
+  matchType: OfferMatchType.direct,
+  price: zero,
+  kind: OfferType.buy,
+  gasPrice: zero,
+};
+
+stories.add('Notification - offer make', () => (
+  <Notification {...{ ...tx, meta: { ...offerMake, args: offerMakeData } }} />
+));
+
+stories.add('Notification - offer make direct', () => (
+  <Notification
+    {...{
+      ...tx,
+      meta: { ...offerMakeDirect, args: offerMakeDirectData },
+    }}
+  />
+));
+
+stories.add('Notification - wrap', () => (
+  <Notification {...{ ...tx, meta: { ...wrap, args: { amount: zero } } }} />
+));
+
+stories.add('Notification - unwrap', () => (
+  <Notification {...{ ...tx, meta: { ...unwrap, args: { amount: zero } } }} />
+));
