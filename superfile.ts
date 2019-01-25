06be2863e6e924bb9c82f66a903ad81bbbe915e9
Krzysztof Kaczor <krzkaczor@gmail.com>
2019-01-25 09:23:45 +0100

    add vis reg to super-ci

--- superfile.ts
+++ superfile.ts
@@ -2,6 +2,9 @@ const { join } = require("path");
 
 const { superCI, report } = require("super-ci");
 const { buildSize } = require("build-size-super-plugin");
+const exec = require("await-exec");
+
+const artifactsPath = "https://s3-eu-west-1.amazonaws.com/superci-bucket/e9344e85-7b81-417e-9b65-4f31253a32d7/";
 
 module.exports.main = async function main() {
   await buildSize({
@@ -15,9 +18,28 @@ async function deploy(path) {
   if (superCI.isPr()) {
     await superCI.saveCollection("build", path);
     report(
-      `Branch deployment: https://s3-eu-west-1.amazonaws.com/superci-bucket/e9344e85-7b81-417e-9b65-4f31253a32d7/${
+      `Branch deployment: ${artifactsPath}${
         superCI.context.currentSha
       }/build/index.html`,
+    );
+  }
+}
+
+async function visReg() {
+  const execOptions = { timeout: 100000, cwd: process.cwd(), log: true };
+  await exec("yarn storybook:screenshots", execOptions);
+  await superCI.saveCollection("storybook-vis-reg", join(__dirname, "__screenshots__"));
+
+  if (superCI.isPr()) {
+    await superCI.getCollection("storybook-vis-reg", join(__dirname, ".reg/expected"));
+    await exec("./node_modules/.bin/reg-suit compare", execOptions);
+
+    await superCI.saveCollection("storybook-vis-reg-report", join(__dirname, ".reg"));
+
+    report(
+      `Vis reg report: ${artifactsPath}${
+        superCI.context.currentSha
+      }/storybook-vis-reg-report/index.html`,
     );
   }
 }
