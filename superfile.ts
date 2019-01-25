4ab26c6e79a2e558d98ca6081efa54f226bce5f4
Krzysztof Kaczor <krzkaczor@gmail.com>
2019-01-25 09:09:59 +0100

    add deploy script

--- superfile.ts
+++ superfile.ts
@@ -7,4 +7,17 @@ module.exports.main = async function main() {
   await buildSize({
     path: "./build/static/js",
   });
+
+  await deploy(join(__dirname, "build"));
+}
+
+async function deploy(path) {
+  if (superCI.isPr()) {
+    await superCI.saveCollection("build", path);
+    report(
+      `Branch deployment: https://s3-eu-west-1.amazonaws.com/superci-bucket/e9344e85-7b81-417e-9b65-4f31253a32d7/${
+        superCI.context.currentSha
+      }/build/index.html`,
+    );
+  }
 }
