57bd6a64e47cfd030e9347e4d6ff059b888c1698
Krzysztof Kostałkowicz <krzysztof.kostalkowicz@in4mates.com>
2019-01-25 12:59:29 +0100

    Use Money component with gas cost USD

--- src/utils/gasCost/GasCost.tsx
+++ src/utils/gasCost/GasCost.tsx
@@ -15,15 +15,11 @@ export const GasCost = (props: HasGasEstimation) => {
       return (<span>error</span>);
     case GasEstimationStatus.unset:
     case undefined:
-      return (<span>
-        <Muted>~{new BigNumber(0).toFixed(2)} USD</Muted>
-        <Money value={new BigNumber(0)} token="ETH" style={{ marginLeft: '0.75em' }} />
-      </span>);
     case GasEstimationStatus.calculated:
-      const usd = gasEstimationUsd ? gasEstimationUsd : new BigNumber(0);
-      const eth = gasEstimationEth ? gasEstimationEth : new BigNumber(0);
+      const usd = gasEstimationUsd || new BigNumber(0);
+      const eth = gasEstimationEth || new BigNumber(0);
       return (<span>
-        <Muted>~{usd.toFixed(2)} USD</Muted>
+        <Muted>~<Money value={usd} token="USD" /></Muted>
         <Money value={eth} token="ETH" style={{ marginLeft: '0.75em' }} />
       </span>);
   }
