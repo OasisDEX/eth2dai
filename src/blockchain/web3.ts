3bacfc6ce39d23f03d983bbe43d3c03f62386021
Pex <georgi.h.georgieff@gmail.com>
2019-01-28 16:35:41 +0100

    Able to recognize different providers ( landing page )

--- src/blockchain/web3.ts
+++ src/blockchain/web3.ts
@@ -1,17 +1,23 @@
 import { from, fromEvent, Observable } from 'rxjs';
 import { catchError, filter, map, shareReplay, startWith, switchMap } from 'rxjs/operators';
 import * as Web3 from 'web3';
+import { ProviderIcons } from '../utils/icons/ProviderIcons';
 
 export let web3 : Web3;
 
 export type Web3Status = 'waiting' | 'ready' | 'denied' | 'missing' | 'initializing';
+
+export interface Web3Window {
+  web3?: any;
+  ethereum?: any;
+}
 
 export const web3Status$: Observable<Web3Status> = fromEvent(document, 'visibilitychange').pipe(
   startWith(null),
   filter(() => !web3),
   filter(() => !document.hidden),
   switchMap(() => {
-    const win = window as { web3?: any, ethereum?: any };
+    const win = window as Web3Window;
     if (win.ethereum) {
       web3 = new Web3(win.ethereum);
       return from(win.ethereum.enable()).pipe(
@@ -35,3 +41,75 @@ export const web3Ready$ = web3Status$.pipe(filter(status => status === 'ready'))
 export function setupFakeWeb3ForTesting() {
   web3 = new Web3();
 }
+
+export const getCurrentProviderName = (provider = (window as Web3Window).web3.currentProvider) => {
+  if (provider.isMetaMask) {
+    return {
+      alias:'metamask',
+      fullName:'Metamask',
+      icon:ProviderIcons.METAMASK
+    };
+  }
+
+  if (provider.isTrust) {
+    return {
+      alias:'trust',
+      fullName:'Trust Wallet',
+      icon: ProviderIcons.TRUST
+    };
+  }
+
+  if (provider.isStatus) {
+    return {
+      alias:'status',
+      fullName:'Status',
+      icon: ProviderIcons.STATUS
+    };
+  }
+
+  if (typeof (window as any).SOFA !== 'undefined') {
+    return {
+      alias:'coinbase',
+      fullName:'Coinbase Wallet',
+      icon: ProviderIcons.COINBASE
+    };
+  }
+
+  if (provider.constructor && provider.constructor.name === 'Web3FrameProvider') {
+    return {
+      alias:'parity',
+      fullName:'Parity',
+      icon: ProviderIcons.PARITY
+    };
+  }
+
+  if (provider.isImToken) {
+    return {
+      alias:'imToken',
+      fullName:'imToken',
+      icon: ProviderIcons.IM_TOKEN
+    };
+  }
+
+  if (provider.host && provider.host.indexOf('infura') !== -1) {
+    return {
+      alias:'infura',
+      fullName:'Infura',
+      icon: ProviderIcons.ETHEREUM
+    };
+  }
+
+  if (provider.host && provider.host.indexOf('localhost') !== -1) {
+    return {
+      alias:'self',
+      fullName:'Private Wallet',
+      icon: ProviderIcons.ETHEREUM
+    };
+  }
+
+  return {
+    alias:'other',
+    fullname:'Oher',
+    icon: ProviderIcons.ETHEREUM
+  };
+};
