5a0e7bf8ec63a5a544eb010a0912c8306f30a243
Aneta Rosłan <aneta.roslan@gmail.com>
2019-01-30 17:55:43 +0100

    Mobile version - 1column layout for mobile (ED-173)

--- src/utils/layout/FlexLayoutRow.tsx
+++ src/utils/layout/FlexLayoutRow.tsx
@@ -1,14 +1,9 @@
 import * as React from 'react';
 
+import * as styles from './FlexLayoutRow.scss';
+
 export const FlexLayoutRow = ({ children }: { children: any }) => (
-  <div
-    style={{
-      display: 'flex',
-      marginBottom: '24px',
-      justifyContent: 'space-between',
-      // flexWrap: 'wrap'
-    }}
-  >
+  <div className={styles.flr}>
     {children}
   </div>
 );
