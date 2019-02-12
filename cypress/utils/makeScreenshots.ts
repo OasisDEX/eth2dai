3b591d4c9fa17adc697dd0363efc75c77470627a
Krzysztof Kaczor <krzkaczor@gmail.com>
2019-02-12 18:38:21 +0100

    make e2e vis reg deterministic

--- cypress/utils/makeScreenshots.ts
+++ cypress/utils/makeScreenshots.ts
@@ -1,17 +1,30 @@
+const blackoutStyle = `
+    <style id="cypress-blackout">
+      [data-vis-reg-mask], [data-vis-reg-mask] * {
+        background-color: black !important;
+        color: black !important;
+      }
+    </style>`;
 const commonScreenshotOptions = { capture: "fullPage" };
 
 export function makeScreenshots(name: string): void {
+  cy.get("html > head").then(e => {
+    e.append(blackoutStyle);
+  });
+
   // if we are in interactive mode just do one screenshot to speed up development cycle
   if ((Cypress.config() as any).isInteractive) {
-    cy.screenshot(commonScreenshotOptions as any);
-    return;
+    cy.screenshot(name, commonScreenshotOptions as any);
+  } else {
+    // see: https://docs.cypress.io/api/commands/viewport.html#Argumentsvalues
+    const viewports = ["macbook-15", "iphone-6+", "iphone-6", "iphone-5"];
+
+    for (const viewport of viewports) {
+      cy.viewport(viewport as any);
+
+      cy.screenshot(`${name}-${viewport.replace("+", "plus")}`, commonScreenshotOptions as any);
+    }
   }
 
-  // see: https://docs.cypress.io/api/commands/viewport.html#Argumentsvalues
-  const viewports = ["macbook-15", "iphone-6+", "iphone-6", "iphone-5"];
-
-  for (const viewport of viewports) {
-    cy.viewport(viewport as any);
-    cy.screenshot(`${name}-${viewport.replace("+", "plus")}`, commonScreenshotOptions as any);
-  }
+  cy.get("#cypress-blackout").then(e => e.remove());
 }
