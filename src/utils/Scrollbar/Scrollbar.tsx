81fa892f1ab843f139d6cd9fde3b068849dbabc6
Pex <georgi.h.georgieff@gmail.com>
2019-02-15 12:48:23 +0100

    Allow scrollable area to be centered against some element. Center spread indicator in Orderbook.

--- src/utils/Scrollbar/Scrollbar.tsx
+++ src/utils/Scrollbar/Scrollbar.tsx
@@ -3,10 +3,22 @@ import { default as Scrollbars } from 'react-custom-scrollbars';
 import * as styles from './Scrollbar.scss';
 
 export class Scrollbar extends React.Component {
+
+  private scroll = React.createRef<Scrollbars>();
+
+  public center(elementOffset:number, elementHeight: number) : void {
+    if (this.scroll.current) {
+      const { clientHeight } = this.scroll.current.getValues();
+      this.scroll.current.scrollTop(elementOffset - ((clientHeight - elementHeight) / 2));
+
+    }
+  }
+
   public render() {
 
     return (
       <Scrollbars
+        ref = {this.scroll}
         renderThumbVertical={(props:any) => <div {...props} className={styles.scrollbarThumb}/>}
         renderThumbHorizontal={(props:any) => <div {...props} className={styles.scrollbarThumb}/>}
       >
