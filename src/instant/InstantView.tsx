d16c623692d62f5fe9207468dcc8052a5b2c2852
Krzysztof Kostałkowicz <krzysztof.kostalkowicz@in4mates.com>
2019-02-19 12:20:11 +0100

    Add instant tab

---
+++ src/instant/InstantView.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export class InstantView extends React.Component {
+  public render() {
+    return <h1>Instant</h1>;
+  }
+}
+
+export const InstantViewTxRx = InstantView;
