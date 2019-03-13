b091da3ad911428f3426b1e3401924e57d491c5b
Kuba W <fenris85@gmail.com>
2019-03-13 15:48:53 +0100

    Remove unnecessary interface

--- src/balances/taxExporter.ts
+++ src/balances/taxExporter.ts
@@ -1,4 +1,3 @@
-import { BigNumber } from 'bignumber.js';
 import { curry } from 'lodash';
 import { combineLatest, Observable } from 'rxjs';
 import { flatMap, switchMap } from 'rxjs/operators';
@@ -6,18 +5,6 @@ import { proxyAddress$ } from '../blockchain/calls/instant';
 import { NetworkConfig } from '../blockchain/config';
 import { vulcan0x } from '../blockchain/vulcan0x';
 import { web3 } from '../blockchain/web3';
-
-export interface TaxExportItem {
-  buyAmount: BigNumber;
-  buyToken: string;
-  sellAmount: BigNumber;
-  sellToken: string;
-  tx: string;
-  source: string;
-  timestamp: Date;
-}
-
-export type TaxExport = TaxExportItem[];
 
 function queryTrades(context: NetworkConfig, addresses: string[]) {
   return vulcan0x<any>(
@@ -53,7 +40,7 @@ function queryTrades(context: NetworkConfig, addresses: string[]) {
 export function createTaxExport$(
   context$: Observable<NetworkConfig>,
   address$: Observable<string>
-) { // : Observable<TaxExport>
+) {
   return combineLatest(context$, address$).pipe(
     switchMap(([context, address]) => {
       return combineLatest(...context.taxProxyRegistries.map(curry(proxyAddress$)(context, address))).pipe(
