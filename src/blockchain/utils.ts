c4330996504a6d4d725d5c7be94f84e6937bd05c
John Doe <georgi.h.georgieff@gmail.com>
2019-03-14 11:50:00 +0100

    Remove unused method

--- src/blockchain/utils.ts
+++ src/blockchain/utils.ts
@@ -11,12 +11,5 @@ export function amountToWei(amount: BigNumber, token: string): BigNumber {
   return amount.times(new BigNumber(10).pow(precision));
 }
 
-export const addressToBytes32 = (x: string, prefix = true) => {
-  let y = x.replace('0x', '');
-  y = padLeft(y, 64);
-  if (prefix) y = '0x' + y;
-  return y;
-};
-
 export const padLeft = (string: string, chars: number, sign?: string) =>
   Array(chars - string.length + 1).join(sign ? sign : '0') + string;
