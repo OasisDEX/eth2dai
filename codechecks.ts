35042bde231d17bed4a30b9167eef79e54ed075f
Krzysztof Kaczor <krzkaczor@gmail.com>
2019-03-17 01:21:26 +0100

    simple type coverage plugin

--- codechecks.ts
+++ codechecks.ts
@@ -1,49 +1,60 @@
-import { codeChecks } from 'codechecks';
-import { buildSize }  from 'codecheck-build-size';
-import { join } from 'path';
+import { codeChecks } from "codechecks";
+import { buildSize } from "codecheck-build-size";
+import { join } from "path";
+import { lint } from "type-coverage";
 // tslint:disable-next-line
-const exec = require('await-exec') as (cmd: string, opt: any) => Promise<void>;
+const exec = require("await-exec") as (cmd: string, opt: any) => Promise<void>;
 
 export async function main() {
   await buildSize({
     files: [
-      { path: './build/static/js/*.js', maxSize: 1024 },
-      { path: './build/static/css/*.css' },
-    ]
+      { path: "./build/static/js/*.js", maxSize: 1024 },
+      { path: "./build/static/css/*.css" },
+    ],
   });
 
-  await deploy(join(__dirname, 'build'));
+  await deploy(join(__dirname, "build"));
 
   await visReg();
+
+  const res = await lint(join(__dirname, "tsconfig.prod.json"), false, false);
+
+  console.log(JSON.stringify(res));
+  await codeChecks.success({
+    name: "Type Coverage",
+    shortDescription: `Current: ${(res.correctCount / res.totalCount).toFixed(2)}%`,
+  });
 }
 
 async function deploy(path: string) {
   if (codeChecks.isPr()) {
-    await codeChecks.saveCollection('build', path);
+    await codeChecks.saveCollection("build", path);
     await codeChecks.success({
-      name: 'Commit deployment',
-      shortDescription: 'Deployment for commit ready.',
-      detailsUrl: codeChecks.getArtifactLink('/build/index.html'),
+      name: "Commit deployment",
+      shortDescription: "Deployment for commit ready.",
+      detailsUrl: codeChecks.getArtifactLink("/build/index.html"),
     });
   }
 }
 
 async function visReg() {
   const execOptions = { timeout: 100000, cwd: process.cwd(), log: true };
-  await exec('yarn storybook:screenshots', execOptions);
-  await codeChecks.saveCollection('storybook-vis-reg', join(__dirname, '__screenshots__'));
+  await exec("yarn storybook:screenshots", execOptions);
+  await codeChecks.saveCollection("storybook-vis-reg", join(__dirname, "__screenshots__"));
 
   if (codeChecks.isPr()) {
-    await codeChecks.getCollection('storybook-vis-reg', join(__dirname, '.reg/expected'));
-    await exec('./node_modules/.bin/reg-suit compare', execOptions);
+    await codeChecks.getCollection("storybook-vis-reg", join(__dirname, ".reg/expected"));
+    await exec("./node_modules/.bin/reg-suit compare", execOptions);
 
-    await codeChecks.saveCollection('storybook-vis-reg-report', join(__dirname, '.reg'));
+    await codeChecks.saveCollection("storybook-vis-reg-report", join(__dirname, ".reg"));
 
-    const reportData = require('./.reg/out.json');
+    const reportData = require("./.reg/out.json");
     await codeChecks.success({
-      name: 'Visual regression for Storybook',
-      shortDescription: `Changed: ${reportData.failedItems.length}, New: ${reportData.newItems.length}, Deleted: ${reportData.deletedItems.length}`,
-      detailsUrl: codeChecks.getArtifactLink('/storybook-vis-reg-report/index.html'),
+      name: "Visual regression for Storybook",
+      shortDescription: `Changed: ${reportData.failedItems.length}, New: ${
+        reportData.newItems.length
+      }, Deleted: ${reportData.deletedItems.length}`,
+      detailsUrl: codeChecks.getArtifactLink("/storybook-vis-reg-report/index.html"),
     });
   }
 }
