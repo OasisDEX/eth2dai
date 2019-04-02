0c486c4a5d2e20ba12b552f42a4bb641f9fdce5f
Krzysztof Kaczor <krzkaczor@gmail.com>
2019-04-02 17:57:01 +0200

    fix

--- .circleci/install-yarn.sh
+++ .circleci/install-yarn.sh
@@ -9,10 +9,5 @@ nvm install 8
 nvm use 8
 nvm alias default 8
 
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+npm install --global yarn
 
- sudo apt-get update -o Dir::Etc::sourcelist="sources.list.d/yarn.list" \
-        -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"
-
-sudo apt-get install -y yarn
