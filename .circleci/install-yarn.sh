e5637d86df78618e52d129a40ec921336ca8ed20
Krzysztof Kostałkowicz <krzysztof.kostalkowicz@in4mates.com>
2019-04-02 16:18:44 +0200

    Wait for apt-get lock

--- .circleci/install-yarn.sh
+++ .circleci/install-yarn.sh
@@ -12,6 +12,15 @@ nvm alias default 8
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
+date
+echo -n "Waiting for other software managers to finish..."
+while pgrep apt-get >/dev/null 2>&1 ; do
+    echo -n "."
+    sleep 5
+done
+echo ""
+date
+
  sudo apt-get update -o Dir::Etc::sourcelist="sources.list.d/yarn.list" \
         -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"
 
