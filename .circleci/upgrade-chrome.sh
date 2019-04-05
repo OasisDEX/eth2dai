5352d8ef80690174ce391e2bb787b8a1820c0cc7
John Doe <georgi.h.georgieff@gmail.com>
2019-04-05 11:08:22 +0200

    Try to upgrade chrome

---
+++ .circleci/upgrade-chrome.sh
@@ -0,0 +1,11 @@
+# Install
+
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+sudo apt-get update
+sudo apt-get install google-chrome-stable
+
+
+# Update
+
+sudo apt-get --only-upgrade install google-chrome-stable
