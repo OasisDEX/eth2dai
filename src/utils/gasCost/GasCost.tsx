cbdadbbe66c5d2ee277a36a63448f6e92a4b51e5
Krzysztof Kostałkowicz <krzysztof.kostalkowicz@in4mates.com>
2019-04-19 16:07:59 +0200

    Remove readonly gas estimation tooltip for offerMake (E2D-288)

--- src/utils/gasCost/GasCost.tsx
+++ src/utils/gasCost/GasCost.tsx
@@ -1,12 +1,9 @@
 import { BigNumber } from 'bignumber.js';
 import * as React from 'react';
 
-import * as infoSvg from '../../icons/info.svg';
 import { GasEstimationStatus, HasGasEstimation } from '../form';
 import { Money } from '../formatters/Formatters';
-import { SvgImage } from '../icons/utils';
 import { Muted } from '../text/Text';
-import * as styles from './GasCost.scss';
 
 export const GasCost = (props: HasGasEstimation) => {
 
@@ -14,10 +11,11 @@ export const GasCost = (props: HasGasEstimation) => {
 
   switch (gasEstimationStatus) {
     case GasEstimationStatus.calculating:
-      return (<span>...</span>);
+      return <span>...</span>;
     case GasEstimationStatus.error:
-      return (<span>error</span>);
+      return <span>error</span>;
     case GasEstimationStatus.unknown:
+      return <span>-</span>;
     case GasEstimationStatus.unset:
     case undefined:
     case GasEstimationStatus.calculated:
@@ -27,9 +25,6 @@ export const GasCost = (props: HasGasEstimation) => {
         {/* This space fixes this issue https://stackoverflow.com/questions/48704520/bootstrap-why-does-double-click-select-in-one-span-also-select-text-in-another*/}
         <Muted>~<Money value={eth} token="ETH"/></Muted>&nbsp;
         <Money value={usd} token="USD" style={{ marginLeft: '0.75em' }}/>
-        { gasEstimationStatus === GasEstimationStatus.unknown &&
-          <SvgImage image={infoSvg} className={styles.unknownIcon} title="Connect to enable gas cost estimation" />
-        }
       </span>);
   }
 };
