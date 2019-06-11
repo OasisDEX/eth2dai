9844c497af46d5e28992941c804e0722f7ff8ebe
John Doe <georgi.h.georgieff@gmail.com>
2019-06-11 11:58:49 +0200

    Fix e2e tests

--- cypress/pages/TradeSettings.ts
+++ cypress/pages/TradeSettings.ts
@@ -1,9 +1,9 @@
-import { tid } from '../utils';
+import { tid, timeout } from '../utils';
 
 export class TradeSettings {
   public static button = () => cy.get(tid('trade-settings'));
 
   public static back = () => cy.get(tid('back')).click();
 
-  public static slippageLimit = (value: string) => cy.get(tid('slippage-limit')).clear().type(value);
+  public static slippageLimit = (value: string) => cy.get(tid('slippage-limit'), timeout(2000)).type(`{selectall}{backspace}${value}`);
 }
